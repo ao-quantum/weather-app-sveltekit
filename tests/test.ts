88eda5b5837ac6d6b497c77a6c25ef5164af8336
xXQuantumSoulXx <killinggamer4mc@gmail.com>
2023-04-11 13:49:07 +0100

    Add tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,34 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
-});
+test.describe('Weather App', () => {
+    test('index page has expected title', async ({ page }) => {
+        await page.goto('/');
+        await expect(page.getByRole('heading', { name: 'Weather' })).toBeVisible();
+    });
+    
+    test('index page has weather adding form', async ({ page }) => {
+        await page.goto('/');
+        await expect(page.getByPlaceholder('Add Weather')).toBeVisible();
+        await expect(page.getByLabel('Add')).toBeVisible();
+    })
+    
+    test('index page has weather table', async ({page}) => {
+        await page.goto('/');
+        await expect(page.getByRole('table')).toBeVisible();
+        await expect(page.getByRole('row', {name: 'London'})).toBeVisible();
+    })
+    
+    test('index page can add new weather', async ({page}) => {
+        await page.goto('/');
+        
+        // Check enter key works
+        await expect(page.getByPlaceholder('Add Weather').fill('Mumbai')).resolves.toBeFalsy();
+        await expect(page.getByPlaceholder('Add Weather').press('Enter')).resolves.toBeFalsy();
+        await expect(page.getByRole('row', {name: 'Mumbai'})).toBeVisible();
+
+        // Check add button works
+        await expect(page.getByPlaceholder('Add Weather').fill('Sydney')).resolves.toBeFalsy();
+        await expect(page.getByRole('button', {name: 'Add'}).click()).resolves.toBeFalsy();
+        await expect(page.getByRole('row', {name: 'Sydney'})).toBeVisible();
+    });
+})
